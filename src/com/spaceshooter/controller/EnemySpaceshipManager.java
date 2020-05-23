4021454614798ebfd12e67025374ae588fe2b47f
theyuvalshor <10600102+theyuvalshor@users.noreply.github.com>
2020-05-23 17:29:07 +0300

    initialized code for 6 classes

--- src/com/spaceshooter/controller/EnemySpaceshipManager.java
+++ src/com/spaceshooter/controller/EnemySpaceshipManager.java
@@ -1,4 +1,33 @@
 package com.spaceshooter.controller;
 
+import com.spaceshooter.model.EnemySpaceship;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EnemySpaceshipManager {
+    private List<EnemySpaceship> enemySpaceships;
+    private LaserbeamManager enemyLasers;
+
+    public EnemySpaceshipManager(LaserbeamManager enemyLasers) {
+        this.enemySpaceships = new ArrayList<EnemySpaceship>();
+        this.enemyLasers = enemyLasers;
+    }
+
+    public void onTick() {
+
+    }
+
+    public void draw(Graphics graphics) {
+
+    }
+
+    public void addEnemySpaceship(EnemySpaceship spaceship) {
+
+    }
+
+    public void destroyEnemySpaceship(EnemySpaceship spaceship) {
+
+    }
 }
