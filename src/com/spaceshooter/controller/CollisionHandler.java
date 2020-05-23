98eeb502b90fb1124ec866637ee7e26cd50a2ac7
EliadTz7 <eliadtz14@gmail.com>
2020-05-23 18:26:33 +0300

    initialize 3 class

--- src/com/spaceshooter/controller/CollisionHandler.java
+++ src/com/spaceshooter/controller/CollisionHandler.java
@@ -1,5 +1,20 @@
 package com.spaceshooter.controller;
 
+import com.spaceshooter.model.Player;
+
 public class CollisionHandler {
+    private EnemySpaceshipManager enemyManager;
+    private LaserbeamManager enemyLasers;
+    private LaserbeamManager playerLasers;
+    private Player player;
+
+    public CollisionHandler(EnemySpaceshipManager enemyManager, Player player) {
+        this.enemyManager = enemyManager;
+        this.player = player;
+    }
+
+    public void onTick() {
+
+    }
 
 }
