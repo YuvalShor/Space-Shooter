d32a288bcf8bfbf0efb1b4662a8c353d1a4613fd
EliadTz7 <eliadtz14@gmail.com>
2020-05-31 19:55:36 +0300

    importing j buttons images

--- src/com/spaceshooter/view/LeaderboardsPanel.java
+++ src/com/spaceshooter/view/LeaderboardsPanel.java
@@ -3,4 +3,8 @@ package com.spaceshooter.view;
 import javax.swing.*;
 
 public class LeaderboardsPanel extends JPanel {
+
+    private JButton newGameButton;
+    private JButton backToMenuButton;
+    private JButton exitButton;
 }
