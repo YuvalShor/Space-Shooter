019495e5a3fbbf94543ff40edb6cb185b4b4b5c1
EliadTz7 <eliadtz14@gmail.com>
2020-06-02 20:50:13 +0300

    creates 3 panels at leaderboard, create hash map in leaderboard at model

--- src/com/spaceshooter/model/Leaderboard.java
+++ src/com/spaceshooter/model/Leaderboard.java
@@ -1,9 +1,60 @@
 package com.spaceshooter.model;
 
 import java.awt.*;
+import java.util.*;
+import java.lang.*;
+import java.util.List;
 
 public class Leaderboard {
     private String leaderboardFilename;
+    private static HashMap<String,Integer> leaderboardsUsers = new HashMap<String, Integer>();
+
+    public Leaderboard() {
+        leaderboardsUsers.put("Eliad",250);
+        leaderboardsUsers.put("Yuval",350);
+        leaderboardsUsers.put("Oron",1450);
+        leaderboardsUsers.put("Alex",550);
+        leaderboardsUsers.put("a",150);
+        leaderboardsUsers.put("b",170);
+        leaderboardsUsers.put("c",0);
+        leaderboardsUsers.put("d",15);
+        leaderboardsUsers.put("bb",150);
+        leaderboardsUsers.put("dsad",150);
+        leaderboardsUsers.put("aqwe",150);
+        leaderboardsUsers.put("vcxvr",150);
+        leaderboardsUsers.put("vcbcvb",150);
+        leaderboardsUsers.put("Valorant",150);
+        leaderboardsUsers = sortByValue(leaderboardsUsers);
+        System.out.println(leaderboardsUsers.toString());
+    }
+
+    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm)
+    {
+        // Create a list from elements of HashMap
+        List<Map.Entry<String, Integer>> list =
+                new LinkedList<>(hm.entrySet());
+
+        // Sort the list
+        Collections.sort(list, new Comparator<>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2)
+            {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+
+        // put data from sorted list to hashmap
+        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+
+        return temp;
+    }
+
+    public static HashMap<String,Integer> getLeaderboardsUsers() {
+        return leaderboardsUsers;
+    }
 
     public void draw(Graphics graphics){
 
--- src/com/spaceshooter/view/LeaderboardsPanel.java
+++ src/com/spaceshooter/view/LeaderboardsPanel.java
@@ -1,6 +1,9 @@
 package com.spaceshooter.view;
 
+import com.spaceshooter.model.Leaderboard;
+
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,51 +13,117 @@ import java.awt.image.BufferedImage;
 
 public class LeaderboardsPanel extends JPanel {
 
+    private final int numOfLeaderboardsUsers = Leaderboard.getLeaderboardsUsers().size() + 1;
     private JButton backToMenuButton = new JButton();
     private ImageHandler imageHandler = new ImageHandler();
     private BufferedImage menuBackgroundImage = ImageHandler.GetMenuImage("/com/spaceshooter/view/images/menuBackground.jpg");;
     private JLabel leaderboardsTitle = new JLabel(imageHandler.CreateIcon("/com/spaceshooter/view/images/leaderboardsTitle.png"));
     private ImageIcon backToMenuButtonIcon = imageHandler.CreateIcon("/com/spaceshooter/view/images/backToMenuButton.png");
     private ImageIcon backToMenuButtonOpaqueIcon = imageHandler.CreateIcon("/com/spaceshooter/view/images/backToMenuButton_Opaque.png");
-
+    private JLabel[] usernameTable = new JLabel[numOfLeaderboardsUsers];
+    private JLabel[] scoreTable = new JLabel[numOfLeaderboardsUsers];
 
     private BackToMenuButtonClickListener backToMenuButtonClickListener;
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
 
         g.drawImage(menuBackgroundImage, 0, 0, null);
     }
 
     public LeaderboardsPanel(int width, int height) {
 
+        JPanel headerPanel = new JPanel();
+        JPanel tablePanel = new JPanel();
+        JPanel footerPanel = new JPanel();
+
+        headerPanel.setOpaque(false);
+        tablePanel.setBackground(new Color(0,0,0,0));
+        footerPanel.setOpaque(false);
+
+        Insets distFromLeft = new Insets(0,0,0,0);
+        Insets distFromRight = new Insets(0,150,0,0);
+
+        headerPanel.setLayout(new GridBagLayout());
+
+        tablePanel.setLayout(new GridBagLayout());
+
         Dimension dimension = new Dimension(width, height);
         setPreferredSize(dimension);
         setMinimumSize(dimension);
         setMaximumSize(dimension);
-        setLayout(new GridBagLayout());
+        setLayout(new BorderLayout());
+
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
-
-
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.weighty = 0.55;
 
         leaderboardsTitle.setPreferredSize(new Dimension(500,100));
-        add(leaderboardsTitle, gridBagConstraints);
+        //gridBagConstraints.anchor = GridBagConstraints.EAST;
+        gridBagConstraints.insets = distFromRight;
+        headerPanel.add(leaderboardsTitle,gridBagConstraints);
+        gridBagConstraints.insets = new Insets(0,0,0,0);
+        add(headerPanel,BorderLayout.NORTH);
 
-        ///to do Jstring
+
+        JScrollPane scrollPane =  new JScrollPane(tablePanel);
+        scrollPane.setOpaque(false);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        add(scrollPane,BorderLayout.CENTER);
+        gridBagConstraints.anchor = GridBagConstraints.CENTER;
+
+        Font tableTitleFont = new Font("Arial",Font.BOLD,30);
+        Font tableFont = new Font("Arial",Font.BOLD,20);
+
+
+
+        usernameTable[0] = new JLabel("Username");
+        usernameTable[0].setFont(tableTitleFont);
+        usernameTable[0].setPreferredSize(new Dimension(200,50));
+        gridBagConstraints.insets = distFromLeft;
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 1;
+        headerPanel.add(usernameTable[0],gridBagConstraints);
+
+        scoreTable[0] = new JLabel("Score");
+        scoreTable[0].setFont(tableTitleFont);
+        scoreTable[0].setPreferredSize(new Dimension(200,50));
+        gridBagConstraints.insets = distFromRight;
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        headerPanel.add(scoreTable[0],gridBagConstraints);
+        gridBagConstraints.anchor = GridBagConstraints.CENTER;
+
+        for (int i = 1 ; i < numOfLeaderboardsUsers ; i++)
+        {
+            String currentKey = (String) Leaderboard.getLeaderboardsUsers().keySet().toArray()[i - 1];
+
+
+            usernameTable[i] = new JLabel(String.valueOf(Leaderboard.getLeaderboardsUsers().get(currentKey)));
+            usernameTable[i].setFont(tableFont);
+            usernameTable[i].setPreferredSize(new Dimension(200,50));
+            gridBagConstraints.insets = distFromLeft;
+            gridBagConstraints.gridx = 2;
+            gridBagConstraints.gridy = i+1;
+            tablePanel.add(usernameTable[i],gridBagConstraints);
+
+            scoreTable[i] = new JLabel(String.valueOf(currentKey));
+            scoreTable[i].setFont(tableFont);
+            scoreTable[i].setPreferredSize(new Dimension(200,50));
+            gridBagConstraints.insets = distFromRight;
+            gridBagConstraints.gridx = 0;
+            gridBagConstraints.gridy = i+1;
+            tablePanel.add(scoreTable[i],gridBagConstraints);
+        }
+
 
         Dimension menuButtonDimension = new Dimension(402, 99);
         backToMenuButton.setPreferredSize(menuButtonDimension);
         backToMenuButton.setIcon(backToMenuButtonIcon);
 
-        //gridBagConstraints.anchor = GridBagConstraints.SOUTH;
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 6;
-        gridBagConstraints.weighty = 0.15;
-        add(backToMenuButton, gridBagConstraints);
+       
+        footerPanel.add(backToMenuButton);
+        add(footerPanel,BorderLayout.SOUTH);
 
         setBorder(BorderFactory.createEtchedBorder());
 
